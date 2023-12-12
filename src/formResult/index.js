09d2c7d4106695db318da2074211410c88f1a7cb
Łukasz Palow <lukaspalow@gmail.com>
2023-12-12 21:24:02 +0100

    formResult

---
+++ src/formResult/index.js
@@ -0,0 +1,8 @@
+import "./styles.css"
+
+export const formResult = () => (
+    <div>
+        <p className="form__text form__text--center"></p>
+        <p className="form__text--special">0</p>
+    </div>
+)
