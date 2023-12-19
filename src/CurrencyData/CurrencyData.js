2fba2f0af6f476041083ad6edbe010757880f0e6
Łukasz Palow <lukaspalow@gmail.com>
2023-12-19 21:52:57 +0100

    new currency table added

---
+++ src/CurrencyData/CurrencyData.js
@@ -0,0 +1,7 @@
+export const CurrencyData = [
+    {country:"Polska", abreviation: "PLN", rate:1},
+    {country:"Europe-Countries", abreviation: "EUR", rate:4.34},
+    {country:"United Stades", abreviation: "USD", rate:4.03},
+    {country:"Norway", abreviation: "NOK", rate:0.37},
+    {country:"Great Britain", abreviation: "GBP", rate:5.07},
+];
