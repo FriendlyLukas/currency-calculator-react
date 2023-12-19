e858ad18c552ff222e94a6018f74bd3bd0c433ac
Łukasz Palow <lukaspalow@gmail.com>
2023-12-19 21:51:51 +0100

    Removing unnecesary components with css

--- src/currencyForm/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-
-export const currencyForm = ({title}) => (
-    <form>
-        <fieldset>
-            <legend>{title}</legend>
-        </fieldset>
-    </form>
-)
--- src/formResult/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles.css"
-
-export const formResult = () => (
-    <div>
-        <p className="form__text form__text--center"></p>
-        <p className="form__text--special">0</p>
-    </div>
-)
