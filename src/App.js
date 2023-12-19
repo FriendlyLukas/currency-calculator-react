cc98ec2666f40c4851614eacf8befeeefbee7fc1
Łukasz Palow <lukaspalow@gmail.com>
2023-12-19 21:53:55 +0100

    correction of brackets

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import Container from "./Container";
 import {Footer} from "./Footer";
-import { currencyTable } from "./currencyTable";
-import {currencyForm} from "./currencyForm"; 
-import { formSelect } from "./formSelect";
-import { formInput } from "./formInput";
-import { formResult } from "./formResult";
+import {Select} from "./Select/Select";
+import { CurrencyData } from "./CurrencyData/CurrencyData";
+
 
 function App() {
   return (
     <Container>
-      <currencyForm title={"Kalkulator walutowy"}>
-      <formSelect/>
-      <formSelect/>
-      <formInput/>
-      <formResult/>
+      <Select CurrencyData={CurrencyData}/>
+      <Select CurrencyData={CurrencyData}/>
       <Footer text={"Kursy średni walut Santander Bank Polska z dnia 28.03.2023"}/>
-      </currencyForm>
     </Container>
   );
 }
--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 
-export const Footer = ({text}) => { (
+export const Footer = ({text}) => { 
+  return (
         <footer className="footer">{text}</footer>
       ); 
 }
