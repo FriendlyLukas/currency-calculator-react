74944caa6d5fb97733bfd745b919129f929a2c4e
Łukasz Palow <lukaspalow@gmail.com>
2023-12-29 19:01:53 +0100

    actualized currency table

---
+++ src/CurrencyData/index.js
@@ -0,0 +1,8 @@
+export const currencyData = [
+    {currency:"złoty polski", abreviation: "PLN", rate: 1},
+    {currency:"euro", abreviation: "EUR", rate: 4.34},
+    {currency:"dolar amerykański", abreviation: "USD", rate: 4.03},
+    {currency:"korona norweska", abreviation: "NOK", rate: 0.37},
+    {currency:"funt brytyjski", abreviation: "GBP", rate: 5.07},
+];
+
